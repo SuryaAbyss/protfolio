f955fba301922794bd8ec6bab068620e7dbb0059
Surya Prakash Subudhiray <168889929+SuryaAbyss@users.noreply.github.com>
2025-08-16 04:49:42 +0530

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -126,7 +126,7 @@ const About = () => {
             className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] hover:underline focus:underline cursor-pointer"
             style={{ zIndex: 20 }}
           >
-            A few songs I can recommend if you're looking for some fresh tunes :)
+            A few songs I can recommend if you are looking for some fresh tunes :)
           </a>
         </div>
       </div>
